5d02952b2cbe26dc46bc6c3b1e60ecd9665e0c44
potakaaa <helbirog@gmail.com>
2025-05-09 20:45:53 +0800

    feat(main menu): initialized main menu screen

---
+++ core/src/main/java/com/throng/game/MainMenuScreen.java
@@ -0,0 +1,128 @@
+package com.throng.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+public class MainMenuScreen implements Screen {
+    private final ThrongGame game;
+    private final OrthographicCamera camera;
+    private Viewport viewport;
+    private Stage stage;
+
+    private Texture backgroundTexture;
+
+    public MainMenuScreen(ThrongGame game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(640, 480, camera);
+        camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+
+        // UI Stage
+        stage = new Stage(viewport, game.batch);
+        Gdx.input.setInputProcessor(stage);
+
+        // Load bg
+        backgroundTexture = new Texture(Gdx.files.internal("background/background_1/background 1.png"));
+
+        // UI elements
+        createUI();
+    }
+
+    private void createUI() {
+        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
+        // Table for layout
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        // Append to table with spacing
+        table.add(playButton).width(200).height(60).padBottom(20);
+        table.row();
+        table.add(exitButton).width(200).height(60);
+
+        // Listeners
+        playButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                // TODO: Change to the game screen when implemented
+                Gdx.app.log("MainMenuScreen", "Play button clicked");
+            }
+        });
+
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Update cam
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        // Draw bg
+        game.batch.begin();
+        game.batch.draw(backgroundTexture, 0, 0, 640, 480);
+        game.batch.end();
+
+        // Draw UI
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        backgroundTexture.dispose();
+        stage.dispose();
+    }
+
+}
--- core/src/main/java/com/throng/game/ThrongGame.java
+++ core/src/main/java/com/throng/game/ThrongGame.java
@@ -1,34 +1,36 @@
 package com.throng.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class ThrongGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
+
+// Main game class that manages the game state and logic
+
+public class ThrongGame extends Game {
+    public SpriteBatch batch;
+    public AssetManager assetManager;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+        assetManager = new AssetManager();
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        setScreen(new MainMenuScreen(this));
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        assetManager.dispose();
     }
 }
